579158b9e002aec6a0c5f96a8fd0c854528c560e
Cyrill N Kalita <cyrill.kalita@gmail.com>
2024-03-18 22:49:20 -0500

    Add test for resolving role name from property

---
+++ tests/Fixtures/FooNamedRole.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace BinaryCats\Rbac\Tests\Fixtures;
+
+use BinaryCats\Rbac\DefinedRole;
+
+class FooNamedRole extends DefinedRole
+{
+    /** @var string  */
+    protected string $name = 'Bar Bar Role';
+}
--- tests/Jobs/SyncDefinedRolesTest.php
+++ tests/Jobs/SyncDefinedRolesTest.php
@@ -5,6 +5,7 @@ namespace BinaryCats\Rbac\Tests\Jobs;
 use BinaryCats\Rbac\Actions\SyncDefinedRole;
 use BinaryCats\Rbac\Jobs\SyncDefinedRoles;
 use BinaryCats\Rbac\Tests\Fixtures\FooRole;
+use BinaryCats\Rbac\Tests\Fixtures\FooNamedRole;
 use BinaryCats\Rbac\Tests\TestCase;
 use PHPUnit\Framework\Attributes\Test;
 
@@ -29,5 +30,13 @@ class SyncDefinedRolesTest extends TestCase
             );
 
         SyncDefinedRoles::dispatch();
+    }
+
+    #[Test]
+    public function it_will_resolve_the_name_of_the_role_if_set_as_a_property(): void
+    {
+        $role = new FooNamedRole();
+
+        $this->assertEquals('Bar Bar Role', $role->name());
     }
 }
